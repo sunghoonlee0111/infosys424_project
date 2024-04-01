3b5e0170798988c6f4d9e7de32c3eee359e9d6b6
Kangyoo Choi <kchoi66@wisc.edu>
2024-04-01 12:43:44 -0500

    Refactor navigation bar and add app.js script

---
+++ public/app.js
@@ -0,0 +1,29 @@
+// navigation burger
+let burger_nav = document.querySelector("#burger_nav");
+let menu_nav = document.querySelector("#menu_nav");
+
+function burger_open() {
+  burger_nav.classList.add("is-active");
+  menu_nav.classList.add("is-active");
+}
+
+function burger_close() {
+  burger_nav.classList.remove("is-active");
+  menu_nav.classList.remove("is-active");
+}
+
+document.addEventListener("click", function (event) {
+  if (
+    !event.target.closest("#burger_nav") &&
+    burger_nav.classList.contains("is-active")
+  ) {
+    burger_close();
+  }
+});
+
+burger_nav.removeEventListener("mouseover", burger_open);
+burger_nav.addEventListener("click", function (event) {
+  event.stopPropagation();
+  burger_nav.classList.toggle("is-active");
+  menu_nav.classList.toggle("is-active");
+});
