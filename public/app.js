68f0f22cb9d65be3f93bb77d3a0f995f2eebb0d9
Sunghoon Lee <fprh666@gmail.com>
2024-04-03 22:22:25 -0500

    create signin signup functions

--- public/app.js
+++ public/app.js
@@ -27,3 +27,34 @@ burger_nav.addEventListener("click", function (event) {
   burger_nav.classList.toggle("is-active");
   menu_nav.classList.toggle("is-active");
 });
+
+//singin
+let signin = document.querySelector("#signin_modal");
+let user_signin = document.querySelector("#user_signin");
+let signin_background = document.querySelector("#signin_background");
+
+function signin_open() {
+  signin.classList.add("is-active");
+}
+
+function signin_close() {
+  signin.classList.remove("is-active");
+}
+
+user_signin.addEventListener("click", signin_open);
+signin_background.addEventListener("click", signin_close);
+
+//signup
+let signup = document.querySelector("#signup_modal");
+let user_signup = document.querySelector("#user_signup");
+let signup_background = document.querySelector("#signup_background");
+
+function signup_open() {
+  signup.classList.add("is-active");
+}
+function signup_close() {
+  signup.classList.remove("is-active");
+}
+
+user_signup.addEventListener("click", signup_open);
+signup_background.addEventListener("click", signup_close);
